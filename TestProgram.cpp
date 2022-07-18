43374d0492e525bb6caa3138abb37c9c19eca3b8
Dustin Smith <45208269+NarrowCoconut@users.noreply.github.com>
2022-07-18 16:16:15 -0400

    Initial Upload
    Initial upload of TestProgram

--- TestProgram.cpp
+++ TestProgram.cpp
@@ -12,33 +12,35 @@ using namespace std;
 
 // Instructions from instructor:
 
-// Define code to perform object-oriented programming in main () function of TestProgram.cpp:
-
-// Create a ClassRoom Object.  
-
-
-// Use the ClassRoom Object to create student object by reading student records from the input file “students.txt” and put the student objects in the array of Students which is the data member of ClassRoom object.
-
-
-// Use the ClassRoom Object to sort the array of students by student average score.
-
-
-// Use the ClassRoom Object to display the students sorted by student average score.
-
-
-// Use the ClassRoom Object to sort the array of students by student last name.
-
-
-// Use the ClassRoom Object to display the students sorted by student last name.
-
-
-// Use the ClassRoom Object to display the number of Student objects created.
-
-
-// Use the ClassRoom Object to calculate and display the average score of all students. 
-
-
 // Note 
 // No global non-constant variables should be used. 
 // You can add more data members or member functions for Student and ClassRoom Class if needed.
 // There should be NO stand-alone function, all functions should be member functions of class student or ClassRoom. 
+
+// Define code to perform object-oriented programming in main () function of TestProgram.cpp:
+int main() {
+// Create a ClassRoom Object.  
+    ClassRoom classRoom("CSC-134");
+
+// Use the ClassRoom Object to create student object by reading student records from the input file “students.txt” 
+//    and put the student objects in the array of Students which is the data member of ClassRoom object.
+    classRoom.popArray(ifstream("students.txt"));
+
+// Use the ClassRoom Object to sort the array of students by student average score.
+    classRoom.sortArrayByAvg();
+
+// Use the ClassRoom Object to display the students sorted by student average score.
+    classRoom.displayStudentData();
+
+// Use the ClassRoom Object to sort the array of students by student last name.
+    classRoom.sortArrayByLName();
+
+// Use the ClassRoom Object to display the students sorted by student last name.
+    classRoom.displayStudentData();
+
+// Use the ClassRoom Object to display the number of Student objects created.
+    cout << "Number of students in class: " << classRoom.numOfStudentsInClass() << endl;
+
+// Use the ClassRoom Object to calculate and display the average score of all students. 
+    cout << "Average score of all students: " << classRoom.calcAvg() << endl;
+}
